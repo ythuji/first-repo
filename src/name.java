bfc99c85599875528583be685a75d72480c75f69
yogathuji <it25100078@my.sliit.lk>
2026-07-17 12:16:52 +0530

    thujibranch 2

--- src/name.java
+++ src/name.java
@@ -2,5 +2,7 @@ public class name {
     static void main() {
         System.out.println("hello world");
         System.out.println("thuji branch");
+        System.out.println("thuji bracnh 2"
+        );
     }
 }
